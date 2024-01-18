0f9db563a1c36e4f60b3be8f29cf93b13d71ac63
TSOI <tsoinikolas@gmail.com>
2024-01-18 15:01:30 +0900

    add tetris and other stuff

--- src/script/script.js
+++ src/script/script.js
@@ -16,3 +16,43 @@ window.addEventListener('scroll', function (e) {
     progressBar.style.width = currPercent * 100 + '%';
 });
 
+// animation
+AOS.init({
+            duration: 800,
+            easeing: 'ease-in-out',
+        });
+
+
+// slide
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+const imgA = document.querySelector('#mainImg');
+const imgSlides = document.querySelectorAll('.main_img');
+
+function reSizing() {
+    const aWidth = imgA.offsetWidth; 
+    const aHeight = imgA.offsetHeight; 
+    
+    imgSlides.forEach(element => {
+        if (element.id !== 'mainImg') {
+            element.style.width = aWidth + 'px';
+            element.style.height = aHeight + 'px';
+        }
+    });
+}
+window.addEventListener('resize', reSizing);
+
+// 초기 실행
+reSizing();
